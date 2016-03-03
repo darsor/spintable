18491f18f6d339f0748473ba19190d02261a0ab3
Dana Sorensen <dana.r.sorensen@gmail.com>
2016-03-03 22:04:21 +0000

    decoder changes

--- motor/encoder/decoder.cpp
+++ motor/encoder/decoder.cpp
@@ -1,19 +1,19 @@
 #include <wiringPiSPI.h>
 #include <cstdio>
+#include <cstdlib>
 
 int setup_encoder(unsigned char (&)[10]);
-int read_encoder(unsigned char (&)[10]);
+int read_encoder(unsigned char data[]);
 int clear_encoder(unsigned char (&)[10]);
 
 int main(){
+    system("gpio load spi");
     int feedback=wiringPiSPISetup(0,9600);
     unsigned char data[10] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
     
     setup_encoder(data);
-    read_encoder(data);
     for(int i=0; i<10; i++)
-        printf("%c",data[i]);
-    printf("\n");
+    read_encoder(data);
     clear_encoder(data);
 
     for(int i=0; i<10; i++)
@@ -31,15 +31,18 @@ int setup_encoder(unsigned char (&data)[10]){
         wiringPiSPIDataRW(0, &data[i], 2);
 }
 
-int read_encoder(unsigned char (&data)[10]){
+int read_encoder(unsigned char data[]){
     data[0]=0x60; 
     data[1]=0x00;
     data[2]=0x00;
     data[3]=0x00;
     data[4]=0x00;
 
+    //for(int i=0; i<10; i++)
+        wiringPiSPIDataRW(0, data, 1);
     for(int i=0; i<10; i++)
-        wiringPiSPIDataRW(0, &data[i], 5);
+        printf("%x",data[i]);
+    printf("\n");
 }
 
 int clear_encoder(unsigned char (&data)[10]){
